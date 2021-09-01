87d73145b4362b05a0e6721a9ae87f987250e44f
Stevan <stevan.earl@asu.edu>
2021-09-01 16:29:04 -0700

    feat: update script and file for fall 2021
    - mostly switching to using the same name for both file types

--- generate-source-data.R
+++ generate-source-data.R
@@ -1,11 +1,20 @@
-library(stringi)
-library(tidyverse)
+# library(stringi)
+# library(tidyverse)
+# 
+# tibble(
+#   index = seq(from = 1, to = 100, by = 1),
+#   var1 = rnorm(n = 100, mean = 5, sd = 1),
+#   var2 = stri_rand_strings(n = 100, length = 5),
+#   var3 = stri_rand_strings(n = 100, length = 5)
+# ) %>% 
+#   mutate(var1 = round(var1, 1)) %>% 
+#   write_csv('~/Desktop/delimitedA.csv')
 
-tibble(
-  index = seq(from = 1, to = 100, by = 1),
-  var1 = rnorm(n = 100, mean = 5, sd = 1),
-  var2 = stri_rand_strings(n = 100, length = 5),
-  var3 = stri_rand_strings(n = 100, length = 5)
-) %>% 
-  mutate(var1 = round(var1, 1)) %>% 
-  write_csv('~/Desktop/delimitedA.csv')
+tibble::tibble(
+  date = lubridate::now() + runif(1e3) * 86400,
+  cat1 = sample(letters, 1e3, replace = TRUE),
+  var1 = runif(1e3),
+  cat2 = sample(letters, 1e3, replace = TRUE),
+  var2 = runif(1e3)
+) |>
+readr::write_csv(file = "~/Desktop/matrix.csv")
